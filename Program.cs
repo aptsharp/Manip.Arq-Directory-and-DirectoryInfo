4b8cf3c3677986e425013fc74a14162dd75778ec
Afonso -aptsharp <aff-on@hotmail.com>
2019-12-29 21:44:01 -0300

    Directory e DirectoryInfo - Listando todas as pastas a partir de uma pasta informada

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Manip.Arq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\aff-o\source\repos\Manip.Arq\New";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                //ou
+                //(importado IEnumerable automaticamente) var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+
+                Console.WriteLine("Folders: ");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR!!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Manip.Arq-Directory-and-DirectoryInfo
+#segue a mesma linha do File e FileInfo
+#Directory operações estaticas
+#DirectoryInfo tem operações de instancia 
+#ambos server para manipulação de pastas.
